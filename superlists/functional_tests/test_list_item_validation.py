651a365e361510901e44192c790e276f822303c7
qs <wqs1639@gmail.com>
2016-08-30 18:19:57 +0800

    添加验证测试

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -24,5 +24,34 @@ class ItemValidationTest(FunctionalTest):
 
 
     def test_can_not_add_empty_list_items(self):
-        self.fail('stop!.')           
+        '不能提交为空的事项'
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.RETURN)
+
+        error = self.browser.find_element_by_css_selector('.has_error')
+        self.assertEqual(error.text,"You can't have an empty list item")
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has_error')
+        self.assertEqual(error.text,"You can't have an empty list item")
+
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Mike tea\n')
+
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Mike tea')
+
+
+
+
+
+
+
+
+
     
